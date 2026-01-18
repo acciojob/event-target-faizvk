007273abd16fc70cc378b08b4d79aa953fde1557
Faiz Zubair <faizvk14@gmail.com>
2026-01-18 17:03:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 class EventTarget {
-    constructor() {
-      this.listeners = new Map();
-    }
-  
-    addEventListener(event, callback) {
-      
-    }
-  
-    removeEventListener(event, callback) {
-      
-    }
-  
-    dispatchEvent(event) {
-    
-    }
-    
+  constructor() {
+    this.listeners = new Map();
   }
-  
+
+  addEventListener(event, callback) {
+    if (!this.listeners.has(event)) {
+      this.listeners.set(event, new Set());
+    }
+
+    // Set prevents duplicate callbacks automatically
+    this.listeners.get(event).add(callback);
+  }
+
+  removeEventListener(event, callback) {
+    if (!this.listeners.has(event)) return;
+
+    this.listeners.get(event).delete(callback);
+
+    // Optional cleanup if no listeners remain
+    if (this.listeners.get(event).size === 0) {
+      this.listeners.delete(event);
+    }
+  }
+
+  dispatchEvent(event) {
+    if (!this.listeners.has(event)) return;
+
+    this.listeners.get(event).forEach(callback => {
+      callback();
+    });
+  }
+}
